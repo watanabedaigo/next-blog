fce00264478902ea8309f09d2084859b2dc0720c
渡邊代悟 <wdaigm0801@gmail.com>
2022-10-24 22:16:29 +0900

    Header Hero Footer コンポーネント作成

---
+++ blog/components/hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1>CUBE</h1>
+      <p>アウトプット用サイト</p>
+    </div>
+  )
+}
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,14 +1,17 @@
+import Header from 'components/header'
+import Hero from 'components/hero'
+import Footer from 'components/footer'
+
 export default function Home() {
   return (
     <>
-      <header>HEADER</header>
+      <Header />
+
       <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプット用サイト</p>
-        </div>
+        <Hero />
       </main>
-      <footer>FOOTER</footer>
+      
+      <Footer />
     </>
   )
 }
