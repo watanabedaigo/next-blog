95c41aa2400dcc44ef74090d3b9b3e5238fe9640
渡邊代悟 <wdaigm0801@gmail.com>
2022-10-25 18:57:21 +0900

    全ページ共通部分作成、設定

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import Layout from 'components/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // 共通部分を全ページコンポーネントに当てるために、共通部分であるLayoutコンポーネントで囲う
+  return (
+    <Layout>
+      {/* Layoutコンポーネントの子要素が、childrenpropsとして渡される */}
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- blog/pages/blog/index.js
+++ blog/pages/blog/index.js
@@ -1,6 +1,6 @@
 import Layout from 'components/layout'
 import Hero from 'components/hero'
 
-export default function Home() {
+export default function Blog() {
   return <Hero />
 }
